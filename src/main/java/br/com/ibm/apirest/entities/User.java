89250412fcf1b3d54ccaa080cac9808c77506497
jair freitas <jairpg1993@gmail.com>
2022-10-12 09:41:26 -0300

    H2 database, test profile, JPA

--- src/main/java/br/com/ibm/apirest/entities/User.java
+++ src/main/java/br/com/ibm/apirest/entities/User.java
@@ -1,11 +1,18 @@
 package br.com.ibm.apirest.entities;
 
-import java.io.Serializable;
+import jakarta.persistence.*;
 
+import java.io.Serial;
+import java.io.Serializable;
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 
+    @Serial
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
