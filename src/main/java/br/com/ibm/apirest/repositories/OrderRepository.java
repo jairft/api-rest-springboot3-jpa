ae22bc9513241f7a7d630b2f5e67ff89b7d5cd1b
jair freitas <jairpg1993@gmail.com>
2022-10-12 11:20:10 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/br/com/ibm/apirest/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package br.com.ibm.apirest.repositories;
+
+import br.com.ibm.apirest.entities.Order;
+import br.com.ibm.apirest.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
