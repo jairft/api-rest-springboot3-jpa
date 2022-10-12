a35f307d8c87544048b40d9f08ad4fab9436330d
jair freitas <jairpg1993@gmail.com>
2022-10-12 10:48:34 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/br/com/ibm/apirest/entities/User.java
+++ src/main/java/br/com/ibm/apirest/entities/User.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
@@ -18,6 +21,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -68,6 +74,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
