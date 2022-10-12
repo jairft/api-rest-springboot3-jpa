b2ba72a09a053d2bcd25215eab8a77a29098f526
jair freitas <jairpg1993@gmail.com>
2022-10-12 10:21:47 -0300

    Service layer, component registration

--- src/main/java/br/com/ibm/apirest/recourse/UserRecource.java
+++ src/main/java/br/com/ibm/apirest/recourse/UserRecource.java
@@ -1,19 +1,30 @@
 package br.com.ibm.apirest.recourse;
 
 import br.com.ibm.apirest.entities.User;
+import br.com.ibm.apirest.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserRecource {
 
-        @GetMapping
-        public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Maria", "maria@gmail.com", "999999999", "123456");
-
-        return ResponseEntity.ok().body(u);
+    @Autowired
+    private UserService userService;
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
