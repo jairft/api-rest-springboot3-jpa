d5738dc12bf2b20019eb41873c31c1bda1e2c15b
jair freitas <jairpg1993@gmail.com>
2022-10-13 18:28:40 -0300

    User delete

--- src/main/java/br/com/ibm/apirest/recourse/UserRecource.java
+++ src/main/java/br/com/ibm/apirest/recourse/UserRecource.java
@@ -33,4 +33,9 @@ public class UserRecource {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
         return ResponseEntity.created(uri).body(user);
     }
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
