24131117f83a303377a749f249e50f95f459e89b
jair freitas <jairpg1993@gmail.com>
2022-10-13 10:53:12 -0300

    Product-OrderItem one-to-many association

--- src/main/java/br/com/ibm/apirest/entities/Product.java
+++ src/main/java/br/com/ibm/apirest/entities/Product.java
@@ -1,6 +1,7 @@
 package br.com.ibm.apirest.entities;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -24,6 +25,9 @@ public class Product implements Serializable {
     @ManyToMany
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -79,6 +83,16 @@ public class Product implements Serializable {
     public Set<Category> getCategories() {
         return categories;
     }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for(OrderItem x : items){
+            set.add(x.getOrder());
+        }
+        return set;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
